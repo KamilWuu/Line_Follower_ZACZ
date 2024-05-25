3dd60ef4b22d7755f5a4ba74114a8978f68c7545
KamilWuu <kamilptakiprzygody@gmail.com>
2024-05-25 11:03:23 +0200

    added pid weights for IR sensors

--- zacz_code/include/Sensors.h
+++ zacz_code/include/Sensors.h
@@ -10,7 +10,11 @@
 
 class Sensors{
 private:
-    const uint8_t IR_SENSORS_PINS[20] = {3,7,8,9,10,14,15,16,17,18,19,20,21,35,36,37,38,39,47,48};
+    /*trzeba ustawic piny tak aby odpowiadaly one rzeczywistym czujnikom od lewej strony, a ten jeden przedni jako ostatni*/
+    const uint8_t IR_SENSORS_PINS[20] = {3,7,8,9,10,14,15,16,17,18,19,20,21,35,36,37,38,39,47,48};  
+
+    
+    const int8_t SENSORS_WEIGHTS[20] = {-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,0}; /*PID WEIGHTS*/
     uint8_t measures[20];
 
 public:
