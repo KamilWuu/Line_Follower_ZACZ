c7ad788aca4998339498f1f598d7303bb067d875
KamilWuu <kamilptakiprzygody@gmail.com>
2024-06-16 14:13:59 +0200

    dodano obsluge MPU6050 do pomiaru kata w osi Z

--- zacz_code/src/main.cpp
+++ zacz_code/src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <MPU6050.h>
+#include <Wire.h>
+#include "ESP32TimerInterrupt.h"
 
 #include "DataFrame.h"
 #include "ReceivedData.h"
 #include "defines.h"
-#include "Wire.h"
 #include "Sensors.h"
 #include "Encoder.h"
-#include "ESP32TimerInterrupt.h"
 #include "PID.h"
 
 //Zmienna określająca częstotliwość pomiaru czasu
@@ -36,9 +37,12 @@ uint16_t Right_pwm_value = 0;
 
 int16_t robot_z_position = 0;
 
-unsigned long last_time;
+unsigned long last_time_encoder;
 
-
+MPU6050 mpu;
+unsigned long last_time_mpu;
+volatile bool mpuInterrupt = false; // Flaga przerwania
+bool isMpuWorking = false;
 
 
 void setupWifi()
@@ -91,7 +95,38 @@ void clientRead()
 }
 
 
+void IRAM_ATTR dmpDataReady() {
+    mpuInterrupt = true;
+}
 
+void updateAngle() {
+    int16_t angle;
+    if (mpuInterrupt) {
+        mpuInterrupt = false;
+
+        int16_t gz = mpu.getRotationZ();
+        unsigned long currentTime = millis();
+        float deltaTime = (currentTime - last_time_mpu) / 1000.0;
+        last_time_mpu= currentTime;
+
+        float gyroResolution = mpu.get_gyro_resolution();
+        float gyroZ = gz * gyroResolution;
+        angle += gyroZ * deltaTime;
+
+        // Normalizacja kąta do zakresu 0-359 stopni
+        if (angle>= 360.0) {
+            angle -= 360.0;
+        } else if (angle < 0.0) {
+            angle += 360.0;
+        }
+
+        robot_z_position = angle;
+    }
+
+    Serial.print("Z_ROT = ");
+    Serial.println(robot_z_position);
+    
+}
 
 
 
@@ -102,11 +137,14 @@ void makeMeasuresAndCalculations(){
   
   //tu trzeba potem dodac jeszcze pomiar z imu 
   if(robot_status == 1){
-  Controller.set_pid(received_data.getPID_parameter(K_P),received_data.getPID_parameter(K_I), received_data.getPID_parameter(K_D));
-  Controller.set_base_speed(received_data.getVMax());
-  Controller.regulator(IR_Sensors.getSensorsError());
-  Left_pwm_percent_value=Controller.get_left_percent();
-  Right_pwm_percent_value= Controller.get_right_percent();
+    Controller.set_pid(received_data.getPID_parameter(K_P),received_data.getPID_parameter(K_I), received_data.getPID_parameter(K_D));
+    Controller.set_base_speed(received_data.getVMax());
+    Controller.regulator(IR_Sensors.getSensorsError());
+    Left_pwm_percent_value=Controller.get_left_percent();
+    Right_pwm_percent_value= Controller.get_right_percent();
+    if(isMpuWorking){
+      updateAngle();
+    }
   //PCalculatePWM(IR_Sensors.getSensorsError(), received_data.getPID_parameter(K_P), received_data.getVMax(), &Left_pwm_percent_value, &Right_pwm_percent_value, &Left_pwm_value, &Right_pwm_value);
   }else if(robot_status == 0){
     Controller.set_pid(0,0,0);
@@ -117,7 +155,10 @@ void makeMeasuresAndCalculations(){
     Right_pwm_value = 0;
     Controller.regulator(IR_Sensors.getSensorsError());
     Controller.set_base_speed(0);
+    robot_z_position = 0;
   }
+
+  
 
   data_to_send.setData(robot_status, IR_Sensors.getSensorsMeasures(), Left_pwm_percent_value, Right_pwm_percent_value, Left_enc.get_speed(), Right_enc.get_speed(), robot_z_position, battery );
 
@@ -128,7 +169,7 @@ void GPIOSetup()
 
   pinMode(BATTERY, INPUT);
   pinMode(BUTTON, INPUT);
-
+  pinMode(IMU_INT, INPUT);
   /*MOTORS*/
 
   /*
@@ -148,7 +189,8 @@ void GPIOSetup()
     pinMode(IR_Sensors.getSensorsPins()[i], INPUT);
   }
 
-  // Wire.setPins(SDA_I2C, SCL_I2C);
+  Wire.setPins(SDA_I2C, SCL_I2C);
+ 
 }
 
 /*Funkcja do obsługi liczenia prędkości co określony czas*/
@@ -158,39 +200,50 @@ bool IRAM_ATTR TimerHandler0(void * timerNo){
   return true;
 }
 
+
+
+
 void setup()
 {
   Serial.begin(115200);
   GPIOSetup();
+  Wire.begin();
+  mpu = MPU6050(); // Domyślny adres 0x68
+  mpu.initialize();
+
   //Wywołanie przerwania do wyzwalania pomiaru prędkości
   if (Timer0.attachInterruptInterval(TIMER0_INTERVAL_MS*1000,TimerHandler0))
   {
 		Serial.print(F("Starting  ITimer0 OK, millis() = "));
 		Serial.println(millis());  
-    }
+  }
   
   setupWifi();
-  //Wire.begin();
 
-  last_time = millis();
+
+  last_time_encoder = millis();
+
+
+
+  mpu.initialize();
+  for(int i = 0; i < 10; i++){
+    if (mpu.testConnection()) {
+      isMpuWorking = true;
+      break;
+    }
+  }
+
+  Serial.println("MPU6050 gotowy");
+  mpu.CalibrateGyro();
+
+  attachInterrupt(digitalPinToInterrupt(IMU_INT), dmpDataReady, RISING);
+  mpu.setIntDataReadyEnabled(true);
+  last_time_mpu = millis();
 }
 
 void loop()
 {
 
-  
-/*
-  Serial.print("Enkodery: L:\t");
-  Serial.print(Left_enc.get_speed(),6);
-  Serial.print("\trotations: ");
-  Serial.print(Left_enc.get_rotations());
-  Serial.print(" R:\t");
-  Serial.print(Right_enc.get_speed(),6);
-  Serial.print("\trotations: ");
-  Serial.print(Right_enc.get_rotations());
-  Serial.print("\n");
-
-*/
   if (WiFi.status() != WL_CONNECTED)
   {
     received_data.setInstruction('M');
@@ -212,6 +265,7 @@ void loop()
   makeMeasuresAndCalculations();
 
   client.println(data_to_send.createDataFrame()); // <== wysyla utworzona ramke danych ze struktury data_to_send do clienta (aplikacji Qt)
+  
   //analogWrite(LEFT_PWM, Left_pwm_value);
   //analogWrite(RIGHT_PWM, Right_pwm_value);
  
